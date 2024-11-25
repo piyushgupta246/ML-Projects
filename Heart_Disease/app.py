3acc4c5e31af719417ceaa1612e1bb92ade196ff
Piyush Kumar Gupta <118933503+piyushgupta246@users.noreply.github.com>
2024-11-26 03:16:30 +0530

    done

--- Heart_Disease/app.py
+++ Heart_Disease/app.py
@@ -20,6 +20,10 @@ model.fit(x_train, y_train)
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
